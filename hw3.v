61240083398831dbdc175bb5b8984b0e9252dba2
jjoojjoo123 <jjoojjoo123@yahoo.com.tw>
2020-10-20 17:56:41 +0800

    done

--- hw3.v
+++ hw3.v
@@ -73,11 +73,11 @@ End two_b.
 Variable DOT: D -> D -> D.
 Variable e: D.
 Notation "A ` B" := (DOT A B) (at level 40, left associativity).
-
-Section three_a.
 Hypothesis Associativity : forall a, forall b, forall c, (a ` (b ` c) = (a ` b) ` c).
 Hypothesis Identity : forall a, ((a ` e = a) /\ (e ` a = a)).
 Hypothesis Inverse : forall a, (exists b, ((a ` b = e) /\ (b ` a = e))).
+
+Section three_a.
 
 Lemma l5 : forall a, forall b, forall c, ((a ` b = a ` c) -> b = c).
 Proof.
@@ -85,7 +85,68 @@ intro x.
 intro y.
 intro z.
 intro w.
-rewrite <- Identity.
+
+elim (Identity z).
+intro u.
+intro v.
+rewrite <- v.
+
+elim (Inverse x).
+intro x_inv.
+intro subInv.
+elim subInv.
+intro n.
+intro m.
+rewrite <- m.
+
+rewrite <- (Associativity x_inv x z).
+rewrite <- w.
+
+rewrite (Associativity x_inv x y).
+rewrite m.
+
+elim (Identity y).
+intro p.
+intro q.
+rewrite q.
+reflexivity.
 Qed.
 
 End three_a.
+
+
+Section three_b.
+
+Lemma l6 : forall a, forall b, forall c, (((a ` b = e) /\ (b ` a = e) /\ (a ` c = e) /\ (c ` a = e)) -> b = c).
+Proof.
+intro x.
+intro y.
+intro z.
+intro w.
+elim (Identity y).
+intro u.
+intro v.
+rewrite <- u.
+
+elim w.
+intro i.
+intro j.
+elim j.
+intro k.
+intro l.
+elim l.
+intro m.
+intro n.
+rewrite <- m.
+
+rewrite (Associativity y x z).
+rewrite k.
+
+elim (Identity z).
+intro p.
+intro q.
+rewrite q.
+reflexivity.
+Qed.
+
+End three_b.
